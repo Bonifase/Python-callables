29b160e14dd0ab72527901b3c41e09fa5d45b1cd
Bonifase <orwabonifase@gmail.com>
2019-02-21 08:27:42 +0300

    using python decorators

--- decorators.py
+++ decorators.py
@@ -8,3 +8,18 @@ def my_decorator(f):
 @my_decorator
 def my_function():
     return "My name"
+
+
+class CallCount:
+    def __init__(self, f):
+        self.f = f
+        self.count = 0
+
+    def __init__(self, *args, **kwargs):
+        self.count += 1
+        return self.f(*args, **kwargs)
+
+
+@CallCount
+def hello(name):
+    print('Hello {}'.format(name))
